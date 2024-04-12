b38c7ea2b1b5d12cc9e3c2bc4fec85ff3f8cead3
progress0407 <progress0407@gmail.com>
2024-04-12 23:53:50 +0900

    프로젝트 초기화

---
+++ src/main/kotlin/io/philo/littlepayment/LittlePaymentApplication.kt
@@ -0,0 +1,11 @@
+package io.philo.littlepayment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LittlePaymentApplication
+
+fun main(args: Array<String>) {
+	runApplication<LittlePaymentApplication>(*args)
+}
---
+++ src/test/kotlin/io/philo/littlepayment/LittlePaymentApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.philo.littlepayment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LittlePaymentApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
