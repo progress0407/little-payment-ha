f0a40be9a8acb3a7d5505ea84768bd8f72bef99e
progress0407 <progress0407@gmail.com>
2024-04-21 20:24:02 +0900

    리액트 환경변수 config.js에서 .env로 변경

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,32 +2,31 @@ import React from 'react';
 import './App.css';
 // import * as PortOne from "https://cdn.portone.io/v2/browser-sdk.esm.js";
 import * as PortOne from "@portone/browser-sdk/v2";
-// import { loadPaymentWidget, ANONYMOUS } from '@tosspayments/payment-widget-sdk'
-import config from './config';
+import config from './secret-config';
 
 function App() {
 
   async function requestPayment() {
 
     const response = await PortOne.requestPayment({
-      // Store ID 설정
-      storeId: config.storeId,
-      // 채널 키 설정
-      channelKey: config.channelKey,
+      storeId: (process.env.REACT_APP_STORE_ID || 'default-value'),
+      channelKey: (process.env.REACT_APP_CHANNEL_KEY || 'default-value'),
       paymentId: `payment-${crypto.randomUUID()}`,
       orderName: "나이키 와플 트레이너 2 SD",
       totalAmount: 100,
       currency: "CURRENCY_KRW",
       payMethod: "CARD",
     });
-    }
+  }
     
   return (
     <div className="App">
       <header className="App-header">
         Hello
       </header>
+
       <hr />
+
       <nav>
         <input type="button" onClick={requestPayment} value='payment'></input>
       </nav>
